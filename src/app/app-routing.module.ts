7f4e04395b449f4e87e35a590bce3df4ec2031d7
AlouiOmar97 <omar.aloui@esprit.tn>
2026-02-05 11:57:16 +0100

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListSuggestionComponent } from './core/list-suggestion/list-suggestion.component';
+import { HomeComponent } from './home/home.component';
+import { AddSuggestionComponent } from './core/add-suggestion/add-suggestion.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { DetailsSuggestionComponent } from './core/details-suggestion/details-suggestion.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', redirectTo:'home', pathMatch:'full'},
+  { path:'home', component: HomeComponent},
+  { path:'suggestion/list', component: ListSuggestionComponent },
+  { path:'suggestion/add', component: AddSuggestionComponent },
+  { path:'suggestion/details/:id' , component: DetailsSuggestionComponent},
+  { path:'**', component: NotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,21 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { FormsModule } from '@angular/forms';
 import { ListSuggestionComponent } from './core/list-suggestion/list-suggestion.component';
+import { AddSuggestionComponent } from './core/add-suggestion/add-suggestion.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { DetailsSuggestionComponent } from './core/details-suggestion/details-suggestion.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    ListSuggestionComponent
+    ListSuggestionComponent,
+    AddSuggestionComponent,
+    HomeComponent,
+    NotFoundComponent,
+    DetailsSuggestionComponent
   ],
   imports: [
     BrowserModule,
