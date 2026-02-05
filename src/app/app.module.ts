6dd4e8091033084471fe96381f83e5dcf098fa15
AlouiOmar97 <omar.aloui@esprit.tn>
2026-02-05 10:12:22 +0100

    Manipulation des composants

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { FormsModule } from '@angular/forms';
+import { ListSuggestionComponent } from './core/list-suggestion/list-suggestion.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ListSuggestionComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/core/list-suggestion/list-suggestion.component.ts
@@ -0,0 +1,70 @@
+import { Component } from '@angular/core';
+import { Suggestion } from '../../models/suggestion';
+
+@Component({
+  selector: 'app-list-suggestion',
+  templateUrl: './list-suggestion.component.html',
+  styleUrl: './list-suggestion.component.css'
+})
+export class ListSuggestionComponent {
+  favoriteSuggestions: Suggestion[] = []
+  searchTerm: string = '';
+  suggestions: Suggestion[] = [
+  {
+  id: 1,
+  title: 'Organiser une journée team building',
+  description: 'Suggestion pour organiser une journée de team building pour renforcer les liens entre les membres de l\'équipe.',
+  category: 'Événements',
+  date: new Date('2025-01-20'),
+  status: 'acceptee',
+  nbLikes:10
+  },
+  {
+  id: 2,
+  title: 'Améliorer le système de réservation',
+  description: 'Proposition pour améliorer la gestion des réservations en ligne avec un système de confirmation automatique.',
+  category: 'Technologie',
+  date: new Date('2025-01-15'),
+  status: 'refusee',
+  nbLikes:0
+  },
+  {
+  id: 3,
+  title: 'Créer un système de récompenses',
+  description: 'Mise en place d\'un programme de récompenses pour motiver les employés et reconnaître leurs efforts.',
+  category: 'Ressources Humaines',
+  date: new Date('2025-01-25'),
+  status: 'refusee',
+  nbLikes:0
+  },
+  {
+  id: 4,
+  title: 'Moderniser l\'interface utilisateur',
+  description: 'Refonte complète de l\'interface utilisateur pour une meilleure expérience utilisateur.',
+  category: 'Technologie',
+  date: new Date('2025-01-30'),
+  status: 'en_attente',
+  nbLikes:0
+  },
+  ];
+
+  likeSuggestion(suggestion: Suggestion) {
+    suggestion.nbLikes++;
+  }
+
+  addToFavorites(suggestion: Suggestion) {
+    if (!this.favoriteSuggestions.includes(suggestion)) {
+      this.favoriteSuggestions.push(suggestion);
+    }
+  }
+
+  filteredSuggestions(): Suggestion[] {
+    if (!this.searchTerm) {
+      return this.suggestions;
+    }
+    return this.suggestions.filter(suggestion => 
+      suggestion.title.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
+      suggestion.category.toLowerCase().includes(this.searchTerm.toLowerCase())
+    );
+  }
+}
